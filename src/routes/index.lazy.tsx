8eb6f5f7e2bf7b7dbd27900e341d40ed72e68708
IamAshish2 <karkiaashish9899@gmail.com>
2024-12-14 20:35:16 +0545

    initial commit. learning tanstack routing

---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,17 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+
+export const Route = createLazyFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+    </div>
+  )
+}
+
+
+// eager loading -> loads all the components at once
+// lazy loading -> loads components as needed
